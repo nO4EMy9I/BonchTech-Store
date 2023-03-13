924fd687fba51fdc7fa9bdb2c34f999ca1c209d7
Nik <vnpking@gmail.com>
2023-03-13 22:09:18 +0300

    Initial Commit

---
+++ BonchTech Store/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BonchTech Store
+//
+//  Created by Nikita Voronov on 13.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
